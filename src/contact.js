99e69d4b671f7685cba54578243510201fa96504
Antonio Melena <melenaantonio@gmail.com>
2021-08-08 16:44:30 -0500

    added images folder and cjs for contact

---
+++ src/contact.js
@@ -0,0 +1,32 @@
+export default function contactPage() {
+  const contactDiv = document.createElement("div");
+  contactDiv.setAttribute("id", "contactDiv");
+
+  const contactContainer = document.createElement("div");
+  contactContainer.setAttribute("id", "containerDiv");
+
+  const imageContainer = document.createElement("div");
+  imageContainer.setAttribute("id", "imageDiv");
+
+  const banner = document.createElement("h1");
+  banner.textContent = "Contact Us";
+
+  const streetAddress = document.createElement("p");
+  streetAddress.textContent = "2049 Fillmore St San Francisco, CA 94115";
+
+  const phoneNumber = document.createElement("p");
+  phoneNumber.textContent = "(222)-888 5555";
+
+  const frontImage = document.createElement("img");
+  frontImage.src = "./images/tacoSign.jpg";
+  frontImage.alt = "Neon Taco Sign";
+
+  contactContainer.appendChild(banner);
+  contactContainer.appendChild(streetAddress);
+  contactContainer.appendChild(phoneNumber);
+  imageContainer.appendChild(frontImage);
+  contactDiv.appendChild(contactContainer);
+  contactDiv.appendChild(imageContainer);
+
+  return contactDiv;
+}
